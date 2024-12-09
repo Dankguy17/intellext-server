7d20c0bb51fe4e42a18a3336ef5a730dca688154
anish madishetty <anishmadishetty@gmail.com>
2024-12-08 18:01:01 -0800

    Updated CORS configs

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ const courseRoutes = require('./routes/courses');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:8081', 'http://localhost:3000', 'https://intellext.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Connect to MongoDB
